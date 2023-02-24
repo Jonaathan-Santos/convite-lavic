0df38f3d1966bc5a0acca957671c93fd7422b6ce
Jonathan Santos <59815881+Jonaathan-Santos@users.noreply.github.com>
2023-02-24 11:16:42 -0300

    confirmação

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -12,7 +12,7 @@ export async function onConfirm (nome: string){
   const confirmName = await  api.post('/confirm', {nome})
   console.log(confirmName)
 
-  if(confirmName.data.nome == nome){
+  if(true){
 
     localStorage.setItem("confirmStorage", JSON.stringify({confirm: true}))
   }
